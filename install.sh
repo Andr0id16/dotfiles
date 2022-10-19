cbb495bc44d07d3a65a21bb47184c3a699701274
Andr0id16 <pracheth.thamankar@gmail.com>
2022-10-19 10:08:58 +0530

    Updated Installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 sudo apt install fish
+chsh -S $(which fish)
+curl -sS https://starship.rs/install.sh | sh
 cp ./config ~/.config/fish/config
 cp ./starhsip.toml ~/.config/starship.toml
