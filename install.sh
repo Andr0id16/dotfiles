a5f6d611a39ab736faca1c36badc4881375a9529
Pracheth Thamankar <80408640+Andr0id16@users.noreply.github.com>
2024-01-07 14:33:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo apt install fish
 sudo apt install curl
 chsh -S $(which fish)
 curl -sS https://starship.rs/install.sh | sh
-sudo cp config.fish ~/.config/fish/config.fish
-sudo cp starship.toml ~/.config/starship.toml
+sudo cp config.fish ~/.config/fish
+sudo cp starship.toml ~/.config
